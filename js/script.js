9b2f1e5480eb7a57ba6fc92b7ed7fce0c632eac0
MilleaNa <lylou.point@gmail.com>
2022-11-22 19:57:05 +0100

    ajout des dernières modifs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
+// Initialisation //
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.tabs').tabs();
     $('.scrollspy').scrollSpy();
     $(".dropdown-trigger").dropdown({ hover: true });
+    $('.tooltipped').tooltip();
+    $('.parallax').parallax(); 
+    $('.materialboxed').materialbox();
+    $('.datepicker').datepicker({
+      disableWeekends: true,
+      yearRange: 1
+    });
   });
+
+  new WOW().init()
+  
+  // Carousel //
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+  setInterval(function() {
+   $('.carousel').carousel('next');
+  }, 4000);
+
+  
